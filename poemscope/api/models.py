437ac0f290ca1d8e42993d17b95c4d98d69bccca
v-lize <liuze@mail.ustc.edu.cn>
2018-12-21 19:04:16 +0800

    modify some code

--- poemscope/api/models.py
+++ poemscope/api/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Order(models.Model):
-    id = models.IntegerField(name='id', primary_key=True)
-    image = models.FileField(name='image', upload_to='../images/')
-    tags = models.CharField(name='tags', max_length=1000, null=True)
-    poem = models.CharField(name='poem',max_length=1000, null=True)
+    image = models.FileField(upload_to='')
+    tags = models.TextField(null=True)
+    poem = models.TextField(null=True)
